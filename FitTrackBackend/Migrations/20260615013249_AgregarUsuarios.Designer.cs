aa9e2c221ac0cf4db0ba6f3a5568bbe78b3f6c9e
juanj <juanj@LAPTOP-BS0QDBE1>
2026-06-14 20:33:26 -0500

    Agregar Usuarios tabla

--- FitTrackBackend/Migrations/20260615013249_AgregarUsuarios.Designer.cs
+++ FitTrackBackend/Migrations/20260615013249_AgregarUsuarios.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FitTrackBackend.Migrations
 {
     [DbContext(typeof(GimnasioContext))]
-    partial class GimnasioContextModelSnapshot : ModelSnapshot
+    [Migration("20260615013249_AgregarUsuarios")]
+    partial class AgregarUsuarios
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FitTrackBackend/Migrations/20260615013249_AgregarUsuarios.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FitTrackBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "usuario_id",
+                table: "ejercicios",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nombre = table.Column<string>(type: "text", nullable: false),
+                    correo = table.Column<string>(type: "text", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    fecha_creacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "usuario_id",
+                table: "ejercicios");
+        }
+    }
+}
